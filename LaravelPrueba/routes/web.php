509f3af3d1de276565f1554de3820006de0e39ad
Walker1718 <carlos.aip212@gmail.com>
2020-10-19 23:46:52 -0300

    Limpie el proyecto base

--- LaravelPrueba/routes/web.php
+++ LaravelPrueba/routes/web.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/*Route::get('/', function () {
+    return view('home');
+});*/
+
+Route::view('/','home')->name('home');
+Route::view('contactos','contact')->name('contact');
+
+
+Route::get('/asd', 'AsdController@index');
